d81c0c80d0792c25c723f844bffa8a8c10eb5f46
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-02-26 16:37:44 +0600

    added more headers

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(express.json())
 
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://coruscating-babka-aef469.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header("Access-Control-Allow-Origin", "*");
   next();
 }) 
